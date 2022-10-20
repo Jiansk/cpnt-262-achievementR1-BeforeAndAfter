7aba548564c732bd693b9b2e0fe73dd745a24c5b
Jiansk <jianv909@gmail.com>
2022-10-19 21:37:00 -0600

    files added

---
+++ index.js
@@ -0,0 +1,13 @@
+var myUsername = "Jiansk";
+
+console.log(myUsername.length);
+console.log(myUsername.substring(1, 3));
+console.log(myUsername.slice(2, 4));
+console.log(myUsername.charAt(0));
+console.log(myUsername.charAt(myUsername.length-1));
+console.log(myUsername.indexOf("ia"));
+console.log(myUsername.replace("ia","io"));
+console.log(myUsername.concat('Is','The','Man'));
+console.log(myUsername.startsWith('J'));
+console.log(myUsername.endsWith('k'));
+console.log(myUsername.includes('s'));
